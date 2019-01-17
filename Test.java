62bb5d6a4adae8c63a0858e0ffb0c896fd9db576
janardhanTJ <janardhanthirumalareddy530@gmail.com>
2019-01-17 10:58:34 +0530

    Adding Test.java

--- Test.java
+++ Test.java
@@ -1,11 +1,28 @@
-import java.io.*;
-import java.lang.*;
-class Test
+import java.util.scanner;
+public class caladd{
+pulbic static void main(String args[])
 {
-public static void main(String[] args)
+float a,b,res;
+char choice,ch;
+Scanner scan=new scanner(System.in);
+do
 {
-
-	System.out.println("Hello world");
-	<System.out.afjasdjf;lsad;l
+System.out.println("Enter two numbers\n");
+System.out.println("2.Exit\n");
+System.out.print("3.Enter your choice:");
+choice=scan.next().charAt(0);
+switch(choice)
+{
+case '1': System.out.print("Enter your number:");
+          a=scan.nextFloat();
+          b=scan.nextFloat();
+          res=a+b;
+          System.out.print("Result=" +res);
+          break;
+case '2': System.exit(0);
+          break;
+default : System.out.print("Invalid choice!");
+          break;
 }
-}
+System.out.print("\n----------------------------------\n");
+}while(choice!=2);
